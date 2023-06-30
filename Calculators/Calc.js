de654674210a0f2552157ec3d9d822e89982d659
HuongGiang <giangharada@gmail.com>
2023-07-01 07:28:05 +0900

    upload source code

--- Calculators/Calc.js
+++ Calculators/Calc.js
@@ -1,14 +1,12 @@
 let btn = document.getElementById("btn");
 
-btn.addEventListener('click', function () {
+btn.addEventListener(`click`, function () {
   let billAmount = document.getElementById("bill_amount").value;
 
   let tipPercentage = document.getElementById("tip_percentage").value;
 
-  let tipAmount=  billAmount / tipPercentage;
- 
-  document.getElementById("tip_amount").value = tipAmount
-
+  let tipAmount = (document.getElementById("tip-amount").value =
+    billAmount / tipPercentage);
   document.getElementById("total_bill").value =
     parseFloat(billAmount) + parseFloat(tipAmount);
 });
